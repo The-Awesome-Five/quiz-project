922a290e0749a8cf4009e4bd762c63da7f3f1852
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2024-08-16 14:56:48 +0300

    project-setup

---
+++ quiz-project/src/firebase/config.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcPZ2BpsHEbONXoz8TxslwH0LNDIZpl3g",
+  authDomain: "quizhub-411fd.firebaseapp.com",
+  databaseURL: "https://quizhub-411fd-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "quizhub-411fd",
+  storageBucket: "quizhub-411fd.appspot.com",
+  messagingSenderId: "1045739663248",
+  appId: "1:1045739663248:web:a5b319589c548cbe938d15"
+};
+
+const app = initializeApp(firebaseConfig);
