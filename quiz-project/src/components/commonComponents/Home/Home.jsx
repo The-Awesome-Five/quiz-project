982bc2dc452c3f5d99cd1c268411a9e1b11d19af
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2024-08-18 22:08:55 +0300

    added-btn-animation/home

--- quiz-project/src/components/commonComponents/Home/Home.jsx
+++ quiz-project/src/components/commonComponents/Home/Home.jsx
@@ -1,38 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './Home.css';
 
 const Home = () => {
+
+    useEffect(() => {
+        const buttons = document.querySelectorAll(".create-btn");
+
+        buttons.forEach(button => {
+            button.addEventListener("click", (e) => {
+                e.preventDefault();
+        
+                if (!button.classList.contains("animate")) {
+                    button.classList.add("animate");
+                    setTimeout(() => {
+                        button.classList.remove("animate");
+                    }, 800); 
+                }
+            });
+        });
+    }, []);
+
     return (
         <div className="home-container d-flex justify-content-center align-items-center mt-4">
-            
             <div className="d-flex quiz-section">
                 
-            
                 <div className="createquiz-container d-flex align-items-center">
                     <div className="img-container">
-                        <img src="https://via.placeholder.com/150" alt="image" />
+                        <img src="https://via.placeholder.com/150" alt="Quiz Image" />
                     </div>
-                    <div className="text-container ms-4">
+                    <div className="text-container text-center ms-4">
                         <h3 className="text-center"><strong>Create a quiz</strong></h3>
                         <p>Play for free with</p>
-                        <p>300 participants</p>
+                        <p>your friends</p>
                         <button className="create-btn btn btn-success">Quiz editor</button>
                     </div>
                 </div>
-                
+
                 
                 <div className="createquiz-container d-flex align-items-center ms-4">
                     <div className="img-container">
-                        <img src="https://via.placeholder.com/150" alt="image" />
+                        <img src="https://via.placeholder.com/150" alt="Organization Image" />
                     </div>
-                    <div className="text-container ms-4">
+                    <div className="text-container text-center ms-4">
                         <h3 className="text-center"><strong>Create an organization</strong></h3>
                         <p>with your friends and</p>
                         <p>conquer new knowledge</p>
                         <button className="create-btn btn btn-info">Organization creator</button>
                     </div>
                 </div>
-
             </div>
         </div>
     );
