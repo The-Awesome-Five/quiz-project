43ba06b5cd5818387911864370c3b60b930176d8
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2024-08-19 09:51:49 +0300

    Admin Menu Added to the Header Bar

--- quiz-project/src/components/commonComponents/HeaderBar/HeaderBar.jsx
+++ quiz-project/src/components/commonComponents/HeaderBar/HeaderBar.jsx
@@ -32,7 +32,7 @@ const HeaderBar = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#link">Link</Nav.Link>
+                            <Nav.Link href="/admin">Admin Menu</Nav.Link>
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">
