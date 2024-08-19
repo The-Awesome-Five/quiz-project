f396d32489d662ab386e948f557b493b356d7426
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2024-08-19 16:13:17 +0300

    Using event listener on the whole document for the button effects

--- quiz-project/src/hoc/ButtonEffectsProvider.jsx
+++ quiz-project/src/hoc/ButtonEffectsProvider.jsx
@@ -1,37 +1,29 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 
 export const ButtonEffectsProvider = ({ children }) => {
-    const location = useLocation();
 
     useEffect(() => {
-        const applyButtonEffects = () => {
-            // Delay the query to ensure DOM is fully updated
-            setTimeout(() => {
-                const buttons = document.querySelectorAll("button");
+        const handleButtonClick = (e) => {
+            const button = e.target.closest("button");
 
-                buttons.forEach(button => {
-                    button.addEventListener("click", (e) => {
-                        if (!button.classList.contains("animate")) {
-                            button.classList.add("animate");
-                            setTimeout(() => {
-                                button.classList.remove("animate");
-                            }, 500);
-                        }
-                    });
-                });
-            }, 500); // 100ms delay, adjust as necessary
+            if (button && !button.classList.contains("animate")) {
+                button.classList.add("animate");
+                setTimeout(() => {
+                    button.classList.remove("animate");
+                }, 500);
+            }
         };
 
-        applyButtonEffects();
+        document.addEventListener("click", handleButtonClick);
 
         return () => {
-            const buttons = document.querySelectorAll("button");
-            buttons.forEach(button => {
-                button.replaceWith(button.cloneNode(true));  // Removing event listeners by cloning the node
-            });
+            document.removeEventListener("click", handleButtonClick);
         };
-    }, [location]);
+    }, []);
 
-    return <div>{children}</div>;
+    return (
+        <div>
+            {children}
+        </div>
+    );
 };
