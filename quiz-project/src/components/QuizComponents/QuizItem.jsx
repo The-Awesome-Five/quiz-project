bfe75511c6f9d3a83909fb71c834cf36eda0f8a7
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2024-08-25 16:33:51 +0300

    update: All Quiz View: Finishing Styling

--- quiz-project/src/components/QuizComponents/QuizItem.jsx
+++ quiz-project/src/components/QuizComponents/QuizItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import {Button, Card, ListGroup} from "react-bootstrap";
+import {Card} from "react-bootstrap";
 import {Link} from "react-router-dom";
+
 
 export const QuizItem = ({
                                 quiz,
@@ -11,15 +12,14 @@ export const QuizItem = ({
     const access = quiz.organizationID ? 'organizations' : 'public';
 
     return (
-        <Card border="success" bg="primary" >
+        <Card bg="success" className="shadow-lg p-3 mb-5 bg-white rounded align-items-center text-center" style={{border:"2px solid green"}}>
             <Card.Img variant="top" src={quiz.avatar.includes('http') ?
                 quiz.avatar :
                 'https://img.freepik.com/premium-vector/quiz-logo-with-speech-bubble-icon_149152-811.jpg'}
-                      className="w-100"
-                      style={{ height: '150px', objectFit: 'fill' }}/>
-            <Card.Body>
+                      className="w-75 h-75 object-fill m-1"/>
+            <Card.Body className="align-items-center text-center" style={{border:"2px solid black"}}>
                 <Card.Title>{quiz.name}</Card.Title>
-                <Card.Text>
+                <Card.Text className="w-100">
                     {quiz.description
                         ? quiz.description.length > 100
                             ? quiz.description.slice(0,100)
