aed1b89691e468782932b28b1135931fe517a7cd
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2024-08-25 12:43:03 +0300

    update: All Quiz View: Added Styling

--- quiz-project/src/components/QuizComponents/QuizItem.jsx
+++ quiz-project/src/components/QuizComponents/QuizItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card, ListGroup} from "react-bootstrap";
+import {Button, Card, ListGroup} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 export const QuizItem = ({
@@ -11,12 +11,20 @@ export const QuizItem = ({
     const access = quiz.organizationID ? 'organizations' : 'public';
 
     return (
-        <Card border="primary" style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={quiz.avatar} />
+        <Card border="success" bg="primary" >
+            <Card.Img variant="top" src={quiz.avatar.includes('http') ?
+                quiz.avatar :
+                'https://img.freepik.com/premium-vector/quiz-logo-with-speech-bubble-icon_149152-811.jpg'}
+                      className="w-100"
+                      style={{ height: '150px', objectFit: 'fill' }}/>
             <Card.Body>
                 <Card.Title>{quiz.name}</Card.Title>
                 <Card.Text>
-                    {quiz.description}
+                    {quiz.description
+                        ? quiz.description.length > 100
+                            ? quiz.description.slice(0,100)
+                            : quiz.description
+                        : 'No description available'}
                 </Card.Text>
             </Card.Body>
             <Card.Body>
@@ -26,7 +34,7 @@ export const QuizItem = ({
                         path: `/quizzes/${access}/${categoryKey}/${diffKey}/${quiz.quizId}`
                     }}
                 >
-                    Start Quiz
+                    <button>Start Quiz</button>
                 </Link>
             </Card.Body>
         </Card>
--- quiz-project/src/views/AllQuizzes/AllQuizzes.jsx
+++ quiz-project/src/views/AllQuizzes/AllQuizzes.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {getAllQuizzes} from "../../services/quiz.service.js";
 import {toast} from "react-toastify";
-import {Container, ListGroup} from "react-bootstrap";
+import {Col, Container, ListGroup, Row} from "react-bootstrap";
 
 import {QuizItem} from "../../components/QuizComponents/QuizItem.jsx";
 
@@ -27,22 +27,26 @@ export const AllQuizzes = () => {
 
     return (
         <Container>
+            <Row xs={4} md={4} className="g-1">
                 {Object.entries(quizData).map(([accessKey, accessValue]) =>
                     Object.entries(accessValue).map(([categoryKey, category]) =>
                         Object.entries(category).map(([diffKey, diff]) =>
                             Object.entries(diff).map(([quizKey, quiz]) => {
                                 return (
+                                    <Col key={quizKey}>
                                     <QuizItem
                                         key={quizKey}
                                         quiz={quiz}
                                         categoryKey={categoryKey}
                                         diffKey={diffKey}
                                     />
+                                    </Col>
                                 );
                             })
                         )
                     )
                 )}
+                </Row>
         </Container>
     )
 }
