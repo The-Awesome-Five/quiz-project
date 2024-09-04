cef09dcf05b6a8ac959860016ae77545ad389177
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2024-09-04 21:21:41 +0300

    feat: create quiz animated card in home page

--- quiz-project/src/components/commonComponents/Home/Home.jsx
+++ quiz-project/src/components/commonComponents/Home/Home.jsx
@@ -1,144 +1,167 @@
-import React, {useEffect, useState} from 'react';
-import { getAllQuizzes, getQuizzesByCat } from '../../../services/quiz.service.js';
+import React, { useEffect, useState } from "react";
+import {
+  getAllQuizzes,
+  getQuizzesByCat,
+} from "../../../services/quiz.service.js";
 import { QuizItem } from "../../QuizComponents/QuizItem.jsx";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination } from "swiper/modules";
 import { useNavigate } from "react-router-dom";
 // Import Swiper styles
-import './Home.css';
+import "./Home.css";
 import "swiper/css";
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Home = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const handleCreateQuizOnClick = () => {
+    navigate("/create-quiz");
+  };
 
-    const handleCreateQuizOnClick = () => {
-        navigate('/create-quiz')
+  const handleCreateOrganizationOnClick = () => {
+    navigate("/create-organization");
+  };
+  const [quizData, setQuizData] = useState([]);
+  const [historyQuizData, setHistoryQuizData] = useState([]);
+  const [mathQuizData, setMathQuizData] = useState([]);
+
+  useEffect(() => {
+    const fetchQuizzes = async () => {
+      const quizzes = await getAllQuizzes();
+      const historyQuizzes = await getQuizzesByCat("history");
+      const mathQuizzes = await getQuizzesByCat("math");
+      setMathQuizData(mathQuizzes);
+      setHistoryQuizData(historyQuizzes);
+      setQuizData(quizzes);
+    };
+
+    try {
+      fetchQuizzes();
+    } catch (e) {
+      toast.error(e);
     }
+  }, []);
 
-    const handleCreateOrganizationOnClick = () => {
-        navigate('/create-organization')
-    }
-    const [quizData, setQuizData] = useState([]);
-    const [historyQuizData, setHistoryQuizData] = useState([]);
-    const [mathQuizData, setMathQuizData] = useState([]);
-
-    useEffect(() => {
-
-        const fetchQuizzes = async () => {
-            const quizzes = await getAllQuizzes();
-            const historyQuizzes = await getQuizzesByCat('history');
-            const mathQuizzes = await getQuizzesByCat('math')
-            setMathQuizData(mathQuizzes)
-            setHistoryQuizData(historyQuizzes)
-            setQuizData(quizzes);
-        }
-
-        try {
-          fetchQuizzes();
-        } catch (e) {
-          toast.error(e);
-        }
-    }, []);
-
-    return (
-        <div className="home-container d-flex flex-column justify-content-center align-items-center mt-4">
-            <div className="d-flex quiz-section">
-
-                <div className="createquiz-container d-flex align-items-center">
-                    <div className="img-container">
-                        <img src="../../../../public/img/brain-icon.png" alt="Quiz Image" />
-                    </div>
-                    <div className="text-container text-center ms-4">
-                        <h3 className="text-center"><strong>Create a quiz</strong></h3>
-                        <p>Play for free with</p>
-                        <p>your friends</p>
-                        <button className="btn create-btn btn-success" onClick={handleCreateQuizOnClick}>Quiz creator</button>
-                    </div>
-                </div>
-
-
-                <div className="createquiz-container d-flex align-items-center ms-4">
-                    <div className="img-container">
-                        <img src="../../../../public/img/organization-icon.png" alt="Organization Image" />
-                    </div>
-                    <div className="text-container text-center ms-4">
-                        <h3 className="text-center"><strong>Create an organization</strong></h3>
-                        <p>with your friends and</p>
-                        <p>conquer new knowledge</p>
-                        <button className="btn create-btn btn-info" onClick={handleCreateOrganizationOnClick}>Organization creator</button>
-                    </div>
-                </div>
-            </div>
-
-
-            <div className="game-modes-container row mt-4 text-start">
-                <h4>All quizzes</h4>
-                <Swiper navigation={true} modules={[Navigation]} slidesPerView={6} spaceBetween={10}
-        pagination={{
-          clickable: true,
-        }} className="mySwiper">
-                 {Object.entries(quizData).map(([id, info]) =>
-                    {
-                                return (
-                                    <SwiperSlide key={id}>
-                                    <QuizItem
-                                        key={id}
-                                        quiz={info}
-                                        id={info.id}
-                                    />
-                                    </SwiperSlide>
-                                );
-                            })
-                }   
-      </Swiper>
-
-      <h4>History quizzes</h4>
-                <Swiper navigation={true} modules={[Navigation]} slidesPerView={6} spaceBetween={10}
-        pagination={{
-          clickable: true,
-        }} className="mySwiper">
-                 {Object.entries(historyQuizData).map(([id, info]) =>
-                    {
-                                return (
-                                    <SwiperSlide key={id}>
-                                    <QuizItem
-                                        key={id}
-                                        quiz={info}
-                                        id={info.id}
-                                    />
-                                    </SwiperSlide>
-                                );
-                            })
-                }   
-      </Swiper>
-
-      <h4>Math quizzes</h4>
-                <Swiper navigation={true} modules={[Navigation]} slidesPerView={6} spaceBetween={10}
-        pagination={{
-          clickable: true,
-        }} className="mySwiper">
-                 {Object.entries(mathQuizData).map(([id, info]) =>
-                    {
-                                return (
-                                    <SwiperSlide key={id}>
-                                    <QuizItem
-                                        key={id}
-                                        quiz={info}
-                                        id={info.id}
-                                    />
-                                    </SwiperSlide>
-                                );
-                            })
-                }   
-      </Swiper>
-            
-            </div>
+  return (
+    <div className="home-container d-flex flex-column justify-content-center align-items-center mt-4">
+      <div className="d-flex quiz-section">
+        <div className="card-container d-flex align-items-center ms-4">
+          <div className="circle" style={{ "--clr": "#19444a" }}>
+            <img
+              src="../../../../public/img/question-mark.png"
+              alt="Coca Cola Logo"
+              className="logo"
+            />
+          </div>
+          <div className="content text-start ms-4">
+            <h3 className="text-start mb-3">
+              <strong>Create a quiz</strong>
+            </h3>
+            <p>with your friends and</p>
+            <p>conquer new knowledge</p>
+            <button
+              className="btn create-btn btn-info mt-4"
+              onClick={handleCreateQuizOnClick}
+            >
+              Quiz creator
+            </button>
+          </div>
+          <img
+            src="../../../../public/img/brain-icon.png"
+            alt="Coca Cola Product"
+            className="product_img"
+          />
         </div>
-    );
-}
+
+        <div className="createquiz-container d-flex align-items-center ms-4">
+          <div className="img-container">
+            <img
+              src="../../../../public/img/organization-icon.png"
+              alt="Organization Image"
+            />
+          </div>
+          <div className="text-container text-center ms-4">
+            <h3 className="text-center">
+              <strong>Create an organization</strong>
+            </h3>
+            <p>with your friends and</p>
+            <p>conquer new knowledge</p>
+            <button
+              className="btn create-btn btn-info"
+              onClick={handleCreateOrganizationOnClick}
+            >
+              Organization creator
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="game-modes-container row mt-4 text-start">
+        <h4>All quizzes</h4>
+        <Swiper
+          navigation={true}
+          modules={[Navigation]}
+          slidesPerView={6}
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          className="mySwiper"
+        >
+          {Object.entries(quizData).map(([id, info]) => {
+            return (
+              <SwiperSlide key={id}>
+                <QuizItem key={id} quiz={info} id={info.id} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+
+        <h4>History quizzes</h4>
+        <Swiper
+          navigation={true}
+          modules={[Navigation]}
+          slidesPerView={6}
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          className="mySwiper"
+        >
+          {Object.entries(historyQuizData).map(([id, info]) => {
+            return (
+              <SwiperSlide key={id}>
+                <QuizItem key={id} quiz={info} id={info.id} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+
+        <h4>Math quizzes</h4>
+        <Swiper
+          navigation={true}
+          modules={[Navigation]}
+          slidesPerView={6}
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          className="mySwiper"
+        >
+          {Object.entries(mathQuizData).map(([id, info]) => {
+            return (
+              <SwiperSlide key={id}>
+                <QuizItem key={id} quiz={info} id={info.id} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
