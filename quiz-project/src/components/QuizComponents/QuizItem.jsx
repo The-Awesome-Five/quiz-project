1491958237804e7d61737c2b76fc05e2c8b342fc
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2024-09-07 23:41:25 +0300

    update: quiz item style

--- quiz-project/src/components/QuizComponents/QuizItem.jsx
+++ quiz-project/src/components/QuizComponents/QuizItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Card} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import './QuizItem.css'
 
 
 export const QuizItem = ({
@@ -11,32 +12,20 @@ export const QuizItem = ({
     const access = quiz.organizationID ? 'organisation' : 'public';
 
     return (
-        <Card bg="success" className="shadow-lg p-3 mb-5 bg-white rounded align-items-center text-center" style={{border:"2px solid green"}}>
-            <Card.Img variant="top" src={quiz.avatar && quiz.avatar.includes('http') ?
-                quiz.avatar :
-                'https://img.freepik.com/premium-vector/quiz-logo-with-speech-bubble-icon_149152-811.jpg'}
-                      className="w-75 h-75 object-fill m-1"/>
-            <Card.Body className="align-items-center text-center" style={{border:"2px solid black"}}>
-                <Card.Title>{quiz.name}</Card.Title>
-                <Card.Text className="w-100">
-                    {quiz.description
-                        ? quiz.description.length > 100
-                            ? quiz.description.slice(0,100)
-                            : quiz.description
-                        : 'No description available'}
-                </Card.Text>
-            </Card.Body>
-            <Card.Body>
-                <Link
-                    to={`/quizzes/${id}`}
-                    state={{
-                        path: `/quizzes/${id}`
-                    }}
-                >
-                    <button>Start Quiz</button>
-                </Link>
-            </Card.Body>
-        </Card>
+<Card bg="success" className="quiz-card shadow-lg p-3 mb-5 bg-white rounded text-center">
+    <Card.Img variant="top" 
+              src={quiz.avatar && quiz.avatar.includes('http') ? quiz.avatar : 'https://img.freepik.com/premium-vector/quiz-logo-with-speech-bubble-icon_149152-811.jpg'}
+              className="quiz-card-img"/>
+    <Card.Body className="quiz-card-body text-center">
+        <Card.Title className="quiz-card-title">{quiz.name}</Card.Title>
+    </Card.Body>
+    <Card.Body>
+        <Link to={`/quizzes/${id}`} state={{ path: `/quizzes/${id}` }}>
+            <button className="quiz-button">Start Quiz</button>
+        </Link>
+    </Card.Body>
+</Card>
+
     )
 
 }
