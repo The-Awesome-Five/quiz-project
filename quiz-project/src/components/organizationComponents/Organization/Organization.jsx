e61d698ebeb779d46195560fc8c7ad3b1c4ebdd5
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2024-09-09 01:48:26 +0300

    update: stylization of "my organization" page

--- quiz-project/src/components/organizationComponents/Organization/Organization.jsx
+++ quiz-project/src/components/organizationComponents/Organization/Organization.jsx
@@ -2,12 +2,12 @@ import { useContext } from "react";
 import { AppContext } from "../../../appState/app.context";
 import { useNavigate } from "react-router-dom";
 import React from "react";
+import './Organization.css'
 
 const Organization = () => {
     const { userData } = useContext(AppContext);
     const navigate = useNavigate();
 
-    // Prepare the organization list
     const orgList = userData?.organizations
         ? Object.keys(userData.organizations).map((orgId) => ({
               orgId: userData.organizations[orgId].organizationID,
@@ -18,70 +18,54 @@ const Organization = () => {
 
     return (
         <div>
-            <div className="organizations-title">
-                {orgList.length > 0 ? (
-                    <h2>Your Organizations</h2>
-                ) : (
-                    <h2>You Have No Organizations Yet</h2>
-                )}
-            </div>
-            <div className="d-flex flex-wrap justify-content-around">
-                {orgList.length > 0 ? (
-                    <div className="d-flex flex-row">
-                    {orgList.map((org, index) => (
-                        <div
-                            key={index}
-                            className="organization-box d-flex flex-column align-items-center justify-content-center border m-3"
-                            style={{
-                                width: "150px",
-                                height: "150px",
-                                border: "2px solid black",
-                            }}
-                            onClick={() => navigate(`/organization/${org.orgId}`)}
-                        >
-                            <img
-                                src={org.photoUrl}
-                                alt={`${org.orgName} logo`}
-                                style={{
-                                    width: "80px",
-                                    height: "80px",
-                                    objectFit: "cover",
-                                }}
-                            />
-                            <p className="mt-2 text-flex">{org.orgName}</p>
-                        </div>
-                    ))}
-                    <div
-                    className="organization-box d-flex align-items-center justify-content-center border m-3"
-                    style={{
-                        width: "150px",
-                        height: "150px",
-                        border: "2px solid black",
-                        fontSize: "50px",
-                        cursor: "pointer",
-                    }}
-                    onClick={() => navigate("/create-organization")}
-                >
-                    +
-                </div>
-                </div>
-                ) : (
-                    <div
-                        className="organization-box d-flex align-items-center justify-content-center border m-3"
-                        style={{
-                            width: "150px",
-                            height: "150px",
-                            border: "2px solid black",
-                            fontSize: "50px",
-                            cursor: "pointer",
-                        }}
-                        onClick={() => navigate("/create-organization")}
-                    >
-                        +
-                    </div>
-                )}
-            </div>
+  <div className="organizations-title">
+    {orgList.length > 0 ? (
+      <h2 className=" text-center mt-3">YOUR ORGANIZATION</h2>
+    ) : (
+      <h2>You Have No Organizations Yet</h2>
+    )}
+  </div>
+  <div className="d-flex flex-wrap justify-content-around">
+    {orgList.length > 0 ? (
+      <div className="d-flex flex-row">
+        {orgList.map((org, index) => (
+          <div
+            key={index}
+            className="organization-box d-flex flex-column align-items-center justify-content-center m-3"
+            style={{ width: "150px", height: "150px" }}
+            onClick={() => navigate(`/organization/${org.orgId}`)}
+          >
+            <img
+              src={org.photoUrl}
+              alt={`${org.orgName} logo`}
+              style={{
+                width: "80px",
+                height: "80px",
+                objectFit: "cover",
+              }}
+            />
+            <p className="mt-2 text-flex">{org.orgName}</p>
+          </div>
+        ))}
+        <div
+          className="create-org-box m-3"
+          style={{ width: "150px", height: "150px" }}
+          onClick={() => navigate("/create-organization")}
+        >
+          +
         </div>
+      </div>
+    ) : (
+      <div
+        className="create-org-box m-3"
+        style={{ width: "150px", height: "150px" }}
+        onClick={() => navigate("/create-organization")}
+      >
+        +
+      </div>
+    )}
+  </div>
+</div>
     );
 };
 
