270f623d94c6529eaed7c185d681f21fdaa0fff5
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2024-09-09 00:02:17 +0300

    Feat: added "join room" bar to the home component

--- quiz-project/src/components/commonComponents/Home/Home.jsx
+++ quiz-project/src/components/commonComponents/Home/Home.jsx
@@ -15,6 +15,8 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 const Home = () => {
+  const [roomId, setRoomId] = useState("");
+
   const navigate = useNavigate();
 
   const handleCreateQuizOnClick = () => {
@@ -27,6 +29,14 @@ const Home = () => {
   const [quizData, setQuizData] = useState([]);
   const [historyQuizData, setHistoryQuizData] = useState([]);
   const [mathQuizData, setMathQuizData] = useState([]);
+
+  const handleJoin = () => {
+    if (roomId.length < 1) {
+      return toast.error("Room Id must not be empty!");
+    }
+
+    navigate(`/room/${roomId}`);
+  };
 
   useEffect(() => {
     const fetchQuizzes = async () => {
@@ -47,12 +57,12 @@ const Home = () => {
 
   return (
     <div className="home-container d-flex flex-column justify-content-center align-items-center mt-4">
-      <div className="d-flex quiz-section">
-        <div className="card-container d-flex align-items-center ms-4">
-          <div className="circle" style={{ "--clr": "#19444a" }}>
+      <div className="d-flex quiz-section justify-content-center">
+        <div className="card-container d-flex align-items-center ms-4 me-4">
+          <div className="circle" style={{ "--clr": "#00819b" }}>
             <img
               src="../../../../public/img/question-mark.png"
-              alt="Coca Cola Logo"
+              alt="Quiz Icon"
               className="logo"
             />
           </div>
@@ -71,16 +81,16 @@ const Home = () => {
           </div>
           <img
             src="../../../../public/img/brain-icon.png"
-            alt="Coca Cola Product"
-            className="product_img"
+            alt="Brain Icon"
+            className="product_img ms-2"
           />
         </div>
 
-        <div className="card-container d-flex align-items-center ms-4">
-          <div className="circle" style={{ "--clr": "#19444a" }}>
+        <div className="card-container d-flex align-items-center ms-4 me-4">
+          <div className="circle" style={{ "--clr": "#00819b" }}>
             <img
               src="../../../../public/img/org-icon.png"
-              alt="Coca Cola Logo"
+              alt="Organization Icon"
               className="logo"
             />
           </div>
@@ -99,8 +109,43 @@ const Home = () => {
           </div>
           <img
             src="../../../../public/img/organization-icon.png"
-            alt="Coca Cola Product"
+            alt="Organization Icon"
             className="product_img"
+          />
+        </div>
+
+        <div className="card-container d-flex align-items-center ms-4">
+          <div className="circle" style={{ "--clr": "#00819b" }}>
+            <img
+              src="../../../../public/img/join-room-icon.png"
+              alt="Quiz Icon"
+              className="logo"
+            />
+          </div>
+          <div className="content text-start ms-4">
+            <h3 className="text-start mb-3">
+              <strong>Join a game</strong>
+            </h3>
+            <p>play with your friends</p>
+            
+            <input
+              type="text"
+              value={roomId}
+              className="input-id form-control pin-input ms-5 mt-3"
+              placeholder="Enter Room Id:"
+              onChange={(e) => setRoomId(e.target.value)}
+            />
+            <button
+              className="btn create-btn btn-info mt-2"
+              onClick={handleJoin}
+            >
+              Join
+            </button>
+          </div>
+          <img
+            src="../../../../public/img/pvp-icon.png"
+            alt="Brain Icon"
+            className="product_img ms-3"
           />
         </div>
       </div>
@@ -108,24 +153,28 @@ const Home = () => {
       <div className="game-modes-container row mt-4 text-start">
         <h4>All quizzes</h4>
         <Swiper
-  navigation={true}
-  modules={[Navigation]}
-  slidesPerView={6}
-  spaceBetween={10}
-  pagination={{ clickable: true }}
-  className="mySwiper"
->
-  {Object.entries(historyQuizData).map(([id, info]) => {
-    return (
-      <SwiperSlide key={id}>
-        <div className="quiz-item-container">
-          <QuizItem key={id} quiz={info} id={info.id} className="quiz-item" />
-        </div>
-      </SwiperSlide>
-    );
-  })}
-</Swiper>
-
+          navigation={true}
+          modules={[Navigation]}
+          slidesPerView={6}
+          spaceBetween={10}
+          pagination={{ clickable: true }}
+          className="mySwiper"
+        >
+          {Object.entries(historyQuizData).map(([id, info]) => {
+            return (
+              <SwiperSlide key={id}>
+                <div className="quiz-item-container">
+                  <QuizItem
+                    key={id}
+                    quiz={info}
+                    id={info.id}
+                    className="quiz-item"
+                  />
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
 
         <h4>History quizzes</h4>
         <Swiper
