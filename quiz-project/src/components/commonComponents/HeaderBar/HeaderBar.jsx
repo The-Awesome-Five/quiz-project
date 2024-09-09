a736737aa945bc8dc81038a11da2a1f452707a11
VLADIMIR VLADIMIROV <166706110+the-volgarian@users.noreply.github.com>
2024-09-09 20:54:30 +0300

    update(HeaderBar): displaying user's currency

--- quiz-project/src/components/commonComponents/HeaderBar/HeaderBar.jsx
+++ quiz-project/src/components/commonComponents/HeaderBar/HeaderBar.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState, useEffect } from "react";
 import "./HeaderBar.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import {
   Container,
   Navbar,
@@ -13,24 +13,46 @@ import {
 import { AppContext } from "../../../appState/app.context";
 import React from "react";
 import { toast } from "react-toastify";
+import { getUserDataByUID } from "../../../services/user.service";
 
 const HeaderBar = ({ logout }) => {
   const [loading, setLoading] = useState(true);
-  // const [roomId, setRoomId] = useState('');
 
   const navigate = useNavigate();
   const { userData } = useContext(AppContext);
+  const { uid } = useParams();
+  const [profileData, setProfileData] = useState(null);
 
   useEffect(() => {
     if (userData === null) {
       setLoading(true);
       setTimeout(() => {
         setLoading(false);
-      }, 1000); // Simulate loading
+      }, 1000); 
     } else {
       setLoading(false);
     }
-  }, [userData]);
+
+    if (uid) {
+      getUserDataByUID(uid)
+        .then((data) => {
+          const userProfile = Object.values(data)[0];
+          console.log("User Profile:", userProfile); 
+          if (userProfile) {
+            setProfileData(userProfile);
+          }
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Failed to fetch user data:", error);
+          setLoading(false);
+        });
+    } else {
+      setProfileData(userData);
+      console.log("User Data from Context:", userData); 
+      setLoading(false);
+    }
+  }, [uid, userData]);
 
   const handleOnClickProfile = () => {
     navigate("/profile");
@@ -38,19 +60,8 @@ const HeaderBar = ({ logout }) => {
 
   const handleOnClickButton = (path, e) => {
     e.preventDefault();
-
     navigate(path);
   };
-
-  // const handleJoin = () => {
-
-  //     if(roomId.length < 1){
-
-  //         return toast.error('Room Id must not be empty!');
-  //     }
-
-  //     navigate(`/room/${roomId}`);
-  // }
 
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
@@ -63,70 +74,78 @@ const HeaderBar = ({ logout }) => {
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-        </Navbar.Collapse>
-        {/* <div className="header-container d-flex align-items-center justify-content-between">
-                    <div className='header-bar d-flex'>
-                        <p className='text mb-0 me-2'>Join game?</p>
-                        <input
-                            type="text"
-                            value={roomId}
-                            className="form-control pin-input"
-                            placeholder="Enter Room Id:"
-                            onChange={(e) => setRoomId(e.target.value)}
-                        />
-                        <button className="btn btn-primary m-3" onClick={handleJoin}>Join</button>
-                    </div>
-                </div> */}
-        <Nav.Link
-          href="/all-quizes"
-          className="m-3"
-          onClick={(e) => handleOnClickButton("/all-quizes", e)}
-        >
-          <Button>All quizes</Button>
-        </Nav.Link>
+        <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
 
-        <Nav.Link
-          href="/gaming-modes"
-          className="me-3"
-          onClick={(e) => handleOnClickButton("/gaming-modes", e)}
-        >
-          <Button>Gaming Modes</Button>
-        </Nav.Link>
+        <div className="button-group">
+          <Nav.Link
+            href="/all-quizes"
+            className="m-3"
+            onClick={(e) => handleOnClickButton("/all-quizes", e)}
+          >
+            <Button>All quizes</Button>
+          </Nav.Link>
 
-        <Nav.Link
-          href="/shop"
-          className="me-3"
-          onClick={(e) => handleOnClickButton("/shop", e)}
-        >
-          <Button>Shop</Button>
-        </Nav.Link>
+          <Nav.Link
+            href="/gaming-modes"
+            className="me-3"
+            onClick={(e) => handleOnClickButton("/gaming-modes", e)}
+          >
+            <Button>Gaming Modes</Button>
+          </Nav.Link>
 
-        <Nav.Link
-          href="/about"
-          className=""
-          onClick={(e) => handleOnClickButton("/about", e)}
-        >
-          <Button>About</Button>
-        </Nav.Link>
+          <Nav.Link
+            href="/shop"
+            className="me-3"
+            onClick={(e) => handleOnClickButton("/shop", e)}
+          >
+            <Button>Shop</Button>
+          </Nav.Link>
+
+          <Nav.Link
+            href="/about"
+            className=""
+            onClick={(e) => handleOnClickButton("/about", e)}
+          >
+            <Button>About</Button>
+          </Nav.Link>
+        </div>
 
         <div className="login-section d-flex align-items-center ms-3">
           {loading ? (
             <Spinner animation="border" role="status">
               <span className="visually-hidden">Loading...</span>
             </Spinner>
-          ) : userData ? (
+          ) : profileData ? (
             <NavDropdown
               title={
-                <img
-                  src={
-                    userData.avatarUrl ||
-                    "../../../../public/img/default-user-icon.png"
-                  }
-                  alt="User Avatar"
-                  className="avatar"
-                  style={{ width: "40px", height: "40px", borderRadius: "50%" }}
-                />
+                <div className="d-flex align-items-center">
+                  {profileData.currency && (
+                    <div className="d-flex align-items-center me-2">
+                      <img
+                        src="/img/coin-icon.png" 
+                        alt="Currency Icon"
+                        className="currency-icon"
+                        style={{ width: "20px", height: "20px", marginRight: "5px" }}
+                      />
+                      <span className="currency-display">
+                        {profileData.currency}
+                      </span>
+                    </div>
+                  )}
+                  <img
+                    src={
+                      profileData.avatarUrl ||
+                      "../../../../public/img/default-user-icon.png"
+                    }
+                    alt="User Avatar"
+                    className="avatar"
+                    style={{
+                      width: "40px",
+                      height: "40px",
+                      borderRadius: "50%",
+                    }}
+                  />
+                </div>
               }
               id="user-nav-dropdown"
               align="end"
