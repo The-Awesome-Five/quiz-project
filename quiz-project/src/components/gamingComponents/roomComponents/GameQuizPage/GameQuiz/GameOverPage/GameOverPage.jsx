3f935e80362326e7272e7d0ebcf3255039bbc36a
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2024-09-10 11:56:15 +0300

    update: Game Over - > when the game is draw there will be appropriate page update: User Access - > Admins have Educator Access update: HeaderBar - > users without Admin role can not see the admin menu

--- quiz-project/src/components/gamingComponents/roomComponents/GameQuizPage/GameQuiz/GameOverPage/GameOverPage.jsx
+++ quiz-project/src/components/gamingComponents/roomComponents/GameQuizPage/GameQuiz/GameOverPage/GameOverPage.jsx
@@ -7,7 +7,7 @@ export const GameOverPage = () => {
 
     const location = useLocation();
 
-    const { room } = location.state || {};
+    const {room} = location.state || {};
 
     const players = Object.values(room.players);
 
@@ -16,11 +16,25 @@ export const GameOverPage = () => {
             <Row xs={1} className="text-lg-start">
                 <PlayerStatusBar player={players[0]}/>
             </Row>
-            <Row xs={1} className="d-flex flex-column align-items-center justify-content-center">
-                    <h1>Game Over</h1>
-                    <h2>Winner: {room.game.winner.username}</h2>
-                    <h3>Score: {room.game.winner.score}</h3>
-                </Row>
+
+            {
+                room.game.winner.id !== "draw"
+                    ? (
+                        <Row xs={1} className="d-flex flex-column align-items-center justify-content-center">
+                            <h1>Game Over</h1>
+                            <h2>Winner: {room.game.winner.username}</h2>
+                            <h3>Score: {room.game.winner.score}</h3>
+                        </Row>
+                    )
+                    : (
+                        <Row xs={1} className="d-flex flex-column align-items-center justify-content-center">
+
+                            <h1>Game Over</h1>
+                            <h2>Draw</h2>
+                            <h3>Score: {room.game.winner.score}</h3>
+                        </Row>
+                    )
+            }
 
             <Row style={{paddingLeft: "100px"}} className="p-12 text-lg-end">
                 <PlayerStatusBar player={players[1]}/>
--- quiz-project/src/hoc/EducatorAccess.jsx
+++ quiz-project/src/hoc/EducatorAccess.jsx
@@ -1,15 +1,20 @@
-
-import {useContext} from "react";
+import React, {useContext} from "react";
 import {Navigate, useLocation} from "react-router-dom";
 import {AppContext} from "../appState/app.context.js";
 
-export default function EducatorAccess ({children}) {
+export default function EducatorAccess({children}) {
 
-    const { userData } = useContext(AppContext);
+    const {userData} = useContext(AppContext);
     const location = useLocation();
 
+    if (userData && userData.role === 'Admin') {
+        return (<>
+            {children}
+        </>)
+    }
+
     if (!userData || userData.role !== 'Educator') {
-        return <Navigate replace to="/not-authorised" state={{ from: location }} />
+        return <Navigate replace to="/not-authorised" state={{from: location}}/>
     }
 
     return (
