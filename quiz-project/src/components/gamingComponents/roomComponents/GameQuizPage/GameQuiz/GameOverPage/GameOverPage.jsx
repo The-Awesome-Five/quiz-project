42f7dbd288cac5311161fc79a36b8c0c036c5258
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2024-09-10 11:31:20 +0300

    update: Game Over Page - > Adding Avatars to the Game Over Page

--- quiz-project/src/components/gamingComponents/roomComponents/GameQuizPage/GameQuiz/GameOverPage/GameOverPage.jsx
+++ quiz-project/src/components/gamingComponents/roomComponents/GameQuizPage/GameQuiz/GameOverPage/GameOverPage.jsx
@@ -1,4 +1,7 @@
 import {useLocation} from "react-router-dom";
+import {PlayerStatusBar} from "../../PlayerStatusBar/PlayerStatusBar.jsx";
+import React from "react";
+import {Card, Container, Row} from "react-bootstrap";
 
 export const GameOverPage = () => {
 
@@ -6,14 +9,23 @@ export const GameOverPage = () => {
 
     const { room } = location.state || {};
 
+    const players = Object.values(room.players);
+
     return (
-        <div>
-            {room.game.winner.id === "draw" ? <h1>Draw!</h1> : null}
-            <h1>{room.game.winner.id === 'draw' ? 'Draw' : 'Winner'}: {room.game.winner.username}</h1>
-            <h2>Score: {room.game.winner.score}</h2>
-            <h1>{room.game.winner.id === 'draw' ? 'Draw' : 'Loser'}: {room.game.loser.username}</h1>
-            <h2>Score: {room.game.loser.score}</h2>
-        </div>
+        <Container className="d-flex flex-row justify-content-center text-center align-items-center">
+            <Row xs={1} className="text-lg-start">
+                <PlayerStatusBar player={players[0]}/>
+            </Row>
+            <Row xs={1} className="d-flex flex-column align-items-center justify-content-center">
+                    <h1>Game Over</h1>
+                    <h2>Winner: {room.game.winner.username}</h2>
+                    <h3>Score: {room.game.winner.score}</h3>
+                </Row>
+
+            <Row style={{paddingLeft: "100px"}} className="p-12 text-lg-end">
+                <PlayerStatusBar player={players[1]}/>
+            </Row>
+        </Container>
     )
 
 
